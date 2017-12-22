8542daf708d4ca65c7c10eafabb930afbe69b6b6
eemj <jamiefarrugia@msn.com>
2017-12-22 17:02:00 +0100

    Add stop/delete button functionality.
    - Fix template download percentage.
    - Set a background / smaller font-size for styles.
    - Add TODO messages for checksum (future feature).

--- src/components/download/download.controller.js
+++ src/components/download/download.controller.js
@@ -3,11 +3,13 @@ import { resolve } from 'path'
 import progress from 'request-progress'
 import request from 'request'
 import { remote } from 'electron'
+import { createHash } from 'crypto'
 import fs from 'fs'
 
 const { app, shell } = remote
-const now = (date = new Date()) => date.toTimeString().split(' ').shift()
+const now = (date = new Date()) => date.toTimeString().split(' ')[0]
 const homePath = app.getPath('home')
+const toMB = value => (value / 1024 / 1024).toFixed(2)
 
 class DownloadController {
   constructor ($mdDialog, $scope, $rootScope, AnimeService, DirectoryService) {
@@ -26,6 +28,7 @@ class DownloadController {
 
     const { foundAnimes } = this._animeService
 
+    this.pendingCancel = false
     this.animes = foundAnimes.map(anime => ({
       ...anime,
       ...{
@@ -49,13 +52,23 @@ class DownloadController {
   }
 
   delete (target) {
-    console.log(target)
+    // This shouldn't happen but we'll make sure it doesn't.
+    if (!this._$rootScope.isDownloading) {
+      const anime = this.animes.find(({ link }) => link === target)
+      if (anime) this.animes.splice(this.animes.indexOf(anime), 1)
+    }
   }
 
   start () {
     this.path = this._directoryService.path
     if (!this._$rootScope.isDownloading) {
       this.download()
+    }
+  }
+
+  stop () {
+    if (!this.pendingCancel && this._$rootScope.isDownloading) {
+      this.pendingCancel = true
     }
   }
 
@@ -80,24 +93,56 @@ class DownloadController {
     download.pipe(fsStream)
 
     download.on('progress', ({ percent, speed, size }) => {
+      if (this.pendingCancel) {
+        // TODO: find a way to make this stop button to work.
+        console.log(download)
+        // Resetting values back to default.
+        this.animes[0].totalPercentage = 0
+        this.pendingCancel = false
+        this._$rootScope.isDownloading = false
+        process.nextTick(() => {
+          fsStream.end()
+          if (fs.existsSync(episodeDir)) fs.unlinkSync(episodeDir)
+        })
+      }
       const downloadIndex =
         currentAnime.totalDownloads - currentAnime.episodes.length + percent
       const totalPercentage = downloadIndex / currentAnime.totalDownloads * 100
       this.animes[0] = {
-        ...this.animes[0],
+        ...currentAnime,
         ...{
           totalPercentage,
           download: {
-            fileSize: size.total / 1024 / 1024,
-            currFileSize: size.transferred / 1024 / 1024,
-            currDlSpeed: speed / 1024 / 1024
+            fileSize: toMB(size.total),
+            currFileSize: toMB(size.transferred),
+            currDlSpeed: toMB(speed)
           }
         }
       }
+      console.log(this.animes[0].totalPercentage)
       if (!this._$scope.$$phase) this._$scope.$apply()
     })
 
     download.on('end', () => {
+      // TODO: create an sha256 checksum, that during loading it'll verify our
+      // the current download's integrity and it doesn't match we'll just delete
+      // the found file, and add it as an update.
+      const fileRead = fs.readFileSync(episodeDir)
+      const shaChecksum = createHash('sha256')
+        .update(fileRead)
+        .digest()
+        .toString('hex')
+        .toLowerCase()
+      const md5Checksum = createHash('md5')
+        .update(fileRead)
+        .digest()
+        .toString('hex')
+        .toLowerCase()
+      console.log(
+        `Episode ${episodes[0]} -> SHA256: ${shaChecksum}, MD5 -> ${
+          md5Checksum
+        }`
+      )
       currentAnime.episodes.splice(0, 1)
       this.download()
     })
@@ -110,7 +155,7 @@ class DownloadController {
         fsStream.end()
         if (fs.existsSync(episodeDir)) fs.unlinkSync(episodeDir)
       })
-      // We'll warn the user that something bad happened..
+      // We'll warn the user that something bad happened.
       console.error(`[download] ${now()} -> ${error}`)
       this._$mdDialog.show(
         this._$mdDialog
