6648a026498bb4e07a279f63e0c4b4af971a48fe
Owen Muscat <owemus@Owens-MacBook-Pro.local>
2017-12-24 03:06:30 +0100

    Add default settings

--- src/services/settings.service.js
+++ src/services/settings.service.js
@@ -3,6 +3,11 @@ import * as path from 'path'
 import { app, remote } from 'electron'
 
 const userData = remote.app.getPath('userData') || app.getPath('userData')
+const defaultSettings = {
+  darkMode: true,
+  disableHardwareAcceleration: false,
+  powerSave: false
+}
 
 class SettingsService {
   constructor () {
@@ -10,9 +15,7 @@ class SettingsService {
 
     this.config = path.join(userData, 'settings.json')
 
-    if (!fs.existsSync(this.config)) {
-      fs.writeFileSync(this.config, '{}', { encoding: 'utf-8' })
-    }
+    if (!fs.existsSync(this.config)) this._resetDefault()
 
     this.has = (...args) => this._settings.has(...args)
     this.get = (...args) => this._settings.get(...args)
@@ -24,37 +27,62 @@ class SettingsService {
     this._load()
   }
 
+  _resetDefault () {
+    return fs.writeFileSync(this.config, JSON.stringify(defaultSettings, null, 2), {
+      encoding: 'utf-8'
+    })
+  }
+
   _load () {
     const settings = this.getAll()
-    for (const key in settings) this._settings.set(key, settings[key])
+
+    for (const key in settings) {
+      this._settings.set(key, settings[key])
+    }
+
+    // Check if the user has the default settings and that it's not a modified version
+    // otherwise we'll just add the missing keys in.
+    Object.keys(defaultSettings).forEach(key => {
+      if (!Object.keys(settings).includes(key)) {
+        ;[this, this._settings].forEach(item => {
+          item.set(key, defaultSettings[key])
+        })
+      }
+    })
+
+    console.log(this._settings.entries())
   }
 
   _write () {
     const settings = {}
-    ;[...this._settings].forEach(([key, value]) => { settings[key] = value })
-    console.log('Settings (write) ->', settings)
+    ;[...this._settings].forEach(([key, value]) => {
+      settings[key] = value
+    })
+    console.info('[settings] write -> %o', settings)
     fs.writeFileSync(this.config, JSON.stringify(settings, null, 2), { encoding: 'utf-8' })
   }
 
   set (...args) {
-    console.info('Settings (set) ->', args)
+    console.info('[settings] set -> %o', args)
     this._settings.set(...args)
     this._write()
   }
 
   delete (...args) {
-    console.info('Settings (delete) ->', args)
+    console.info('[settings] delete -> %o', args)
     this._settings.delete(...args)
     this._write()
   }
 
   clear () {
-    console.info('Settings (clear)')
+    console.info('[settings] clear')
     this._settings.clear()
     this._write()
   }
 
-  getAll () { return JSON.parse(fs.readFileSync(this.config, { encoding: 'utf-8' })) }
+  getAll () {
+    return JSON.parse(fs.readFileSync(this.config, { encoding: 'utf-8' }))
+  }
 }
 
 export default SettingsService
