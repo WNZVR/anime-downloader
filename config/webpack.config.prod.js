d4b45f6c43696e7f7a724e6047e03793ac446219
eemj <jamiefarrugia@msn.com>
2017-12-25 01:08:09 +0100

    Delete dist folder before building

--- config/webpack.config.prod.js
+++ config/webpack.config.prod.js
@@ -1,3 +1,14 @@
-const baseConfig = require('./webpack.config.base.js')(true)
+const rimraf = require('rimraf')
+const baseConfig = require('./webpack.config.base')(true)
+const { appDist } = require('./defaults')
+const { existsSync } = require('fs')
+
+// We'll remove it since it creates hashes builds and there will be
+// extra useless files..
+if (existsSync(appDist)) {
+  rimraf(appDist, error => {
+    if (error) throw error
+  })
+}
 
 module.exports = Object.assign(baseConfig, { stats: 'errors-only' })
