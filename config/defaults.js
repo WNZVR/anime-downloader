8787af8ed05c5a33e1e5d14683c65043153ff51e
eemj <jamiefarrugia@msn.com>
2017-12-25 01:07:47 +0100

    Relocate build resources

--- config/defaults.js
+++ config/defaults.js
@@ -2,17 +2,16 @@ const { join } = require('path')
 const resolveApp = args => join(__dirname, `../${args}`)
 
 module.exports = {
-  // We'll let cross-env dependency decide whether the application
-  // is running as production.
-  appProduction: process.env.NODE_ENV === 'production',
+  // We'll only use this for electron's DevTools
+  appProduction: process.env.NODE_ENV === 'production' || 'development',
 
   appDevPort: 4200,
   appDevHostname: 'localhost',
 
   appTitle: 'Anime Downloader',
 
-  appLogoPng: resolveApp('src/assets/images/icon.png'),
-  appLogoNotif: resolveApp('src/assets/images/icon_notification.png'),
+  appLogoPng: resolveApp('resources/icon.png'),
+  appLogoNotif: resolveApp('resources/icon_notification.png'),
 
   appSrc: resolveApp('src'),
   appDist: resolveApp('dist'),
