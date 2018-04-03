d6fb9bc1730873a22543be35215db0818bc47a5f
eemj <jamiefarrugia@msn.com>
2018-04-03 14:19:55 +0200

    Minor bug fixes (updates refreshing, errors)
    - Add error handling when the user decides to remove an anime.
    - Fix anime updates refreshing (after closing dialogs and deleting an
    anime).
    - Cleanup the context menu code abit.

--- src/components/home/home.controller.js
+++ src/components/home/home.controller.js
@@ -7,7 +7,14 @@ import { existsSync, readdirSync } from 'fs'
 const { Menu } = remote
 
 class HomeController {
-  constructor ($state, $mdDialog, $rootScope, AnimeService, SettingsService, DirectoryService) {
+  constructor (
+    $state,
+    $mdDialog,
+    $rootScope,
+    AnimeService,
+    SettingsService,
+    DirectoryService
+  ) {
     'ngInject'
 
     this._$state = $state
@@ -30,9 +37,10 @@ class HomeController {
 
   refreshAnimeUpdates () {
     const { foundAnimes } = this._animeService
-    if (!foundAnimes.length) return
     let updates = 0
-    foundAnimes.forEach(({ episodes }) => (updates += episodes.length))
+    if (foundAnimes.length) {
+      foundAnimes.forEach(({ episodes }) => (updates += episodes.length))
+    }
     ipcRenderer.send('TRAY_ICON', !!updates)
     this._$rootScope.updates = updates
   }
@@ -48,7 +56,9 @@ class HomeController {
           .alert()
           .clickOutsideToClose()
           .title('Error')
-          .textContent('Something went wrong, press \'Refresh\' to restart the application.')
+          .textContent(
+            'Something went wrong, press \'Refresh\' to restart the application.'
+          )
           .ariaLabel('Error')
           .ok('OK')
       )
@@ -59,7 +69,7 @@ class HomeController {
         escapeToClose: true,
         template: '<add></add>',
         parent: angular.element(document.body),
-        onRemoving: this.refreshAnimeUpdates,
+        onRemoving: this.refreshAnimeUpdates.bind(this),
         fullscreen: false
       })
     }
@@ -70,68 +80,70 @@ class HomeController {
       ariaLabel: 'Download',
       template: '<download></download>',
       parent: angular.element(document.body),
-      onClosing: this.refreshAnimeUpdates,
+      onClosing: this.refreshAnimeUpdates.bind(this),
       escapeToClose: false,
       fullscreen: false
     })
   }
 
   changeDirectory () {
-    if (this._directoryService.change()) {
-      this.reload()
-    }
+    if (this._directoryService.change()) this.reload()
   }
 
-  _remove (path, link) {
-    const found = this.foundAnimes.find(anime => anime.link === link)
+  cleanAnime (path, link) {
     if (existsSync(path)) {
-      rimraf(path, error => {
-        if (error) console.error(error)
-        this.foundAnimes.splice(this.foundAnimes.indexOf(found), 1)
+      return rimraf(path, error => {
+        if (error) {
+          return this._$mdDialog.show(
+            this._$mdDialog
+              .alert()
+              .clickOutsideToClose()
+              .title('Error')
+              .textContent(error.stack || error)
+              .ariaLabel('Error')
+              .ok('OK')
+          )
+        }
+        this.foundAnimes.splice(
+          this.foundAnimes.indexOf(
+            this.foundAnimes.find(anime => anime.link === link)
+          ),
+          1
+        )
+        this.refreshAnimeUpdates()
       })
-    }
+    } else this.refreshAnimeUpdates()
   }
 
-  contextMenu (event, link) {
-    const { clientX, clientY, which } = event
-    if (which === 3) {
-      const animePath = join(this.path, link)
-      const menu = Menu.buildFromTemplate([
-        {
-          label: 'Delete',
-          click: () => {
-            if (existsSync(animePath)) {
-              if (readdirSync(animePath).length) {
-                const confirmation = this._$mdDialog
-                  .confirm()
-                  .clickOutsideToClose()
-                  .title('Error')
-                  .textContent(
-                    'Are you sure you want to delete this anime along with it\'s contents?'
-                  )
-                  .ariaLabel('Alert')
-                  .ok('Yes')
-                  .cancel('No')
-
-                this._$mdDialog.ahow(confirmation).then(
-                  () => {
-                    this._remove(animePath, link)
-                  },
-                  () => {}
-                )
-              } else this._remove(animePath, link)
-            } else this._remove(animePath, link)
-          }
-        },
-        {
-          label: 'Open Folder',
-          click: () => {
-            this._directoryService.open(animePath)
+  contextMenu ({ clientX, clientY, which }, link) {
+    if (which !== 3) return false
+    const animePath = join(this.path, link)
+    Menu.buildFromTemplate([
+      {
+        label: 'Delete',
+        click: () => {
+          if (existsSync(animePath)) {
+            if (readdirSync(animePath).length) {
+              const confirm = this._$mdDialog
+                .confirm()
+                .title('Warning')
+                .textContent('Are you sure you want to delete this anime?')
+                .ariaLabel('Alert')
+                .ok('Yes')
+                .cancel('No')
+              return this._$mdDialog
+                .show(confirm)
+                .then(() => this.cleanAnime(animePath, link), () => {})
+            }
+            return this.cleanAnime(animePath, link)
           }
         }
-      ])
-      menu.popup({ x: clientX, y: clientY })
-    }
+      },
+      {
+        label: 'Open Folder',
+        click: () => this._directoryService.open(animePath)
+      }
+    ]).popup({ x: clientX, y: clientY })
   }
 }
 
